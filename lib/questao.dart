251bc90df29d4a9212242ee0e4daa95f04d44fd6
EliweltonMoreira <eliwelton.moreira@outlook.com>
2022-02-26 16:19:04 -0300

    Integrate question widget into tree

---
+++ lib/questao.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+  const Questao(this.texto, {Key? key}) : super(key: key);
+
+  final String texto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(texto);
+  }
+}
